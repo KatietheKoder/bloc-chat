38f7bba3a1d2795f6e60a41973578cb599282b07
KatietheKoder <katieknecht@ymail.com>
2018-09-24 13:55:26 -0400

    List rooms

--- src/Components/RoomList.js
+++ src/Components/RoomList.js
@@ -8,39 +8,49 @@ class RoomList extends Component {
     this.state = {
       rooms:[]
       }
-    }
-    roomsRef = this.props.firebase.database().ref('rooms');
+
+    this.roomsRef = this.props.firebase.database().ref('rooms');
   }
 
-  componentDidMount()
+  componentDidMount() {
 
     this.roomsRef.on('child_added', snapshot => {
          const room = snapshot.val();
          room.key = snapshot.key;
          this.setState({ rooms: this.state.rooms.concat( room ) })
-         );
+
+          });
        }
 
 
   render() {
-    const RoomList = this.state.rooms.map((rooms)=>{
-         return()
-       }
-      )
-     }
-        </ul>
-     </div>
+
+    const RoomForm = (
+
       <form onSubmit={this.createRoom}>
-        <formGroup>
-          <inputGroup>
-            <formControl type="text" name="title" value={this.state.title} placeholder="New Room" onChange={this.handleChange} />
-          <imputGroup.Button>
-            <Button type="submit">Create</Button>
-          </imputGroup.Button>
-        </inputGroup>
-      </FormGroup>
-    </form>
-    }
+       <formGroup>
+         <inputGroup>
+           <formControl type="text" name="title" value={this.state.title} placeholder="New Room" onChange={this.handleChange} />
+           <button type="submit">Create</button>
+       </inputGroup>
+     </formGroup>
+     </form>
+
+    );
+
+      const RoomList = this.state.rooms.map((room)=>{
+      <div key={room.key} onClick={ (e) => {this.selectRoom(room,e)} }> {room.name}.
+      <button className="deleteRoom" onClick= { (e) => {this.deleteRoom(room.key)} }>Delete</button>/>
+
+
+  })
+
+    return (
+      <div className= 'SelectRoom'>
+        <ul>{RoomForm}</ul>
+        <ul>{RoomList}</ul>
+      </div>
+    );
   }
 }
 
