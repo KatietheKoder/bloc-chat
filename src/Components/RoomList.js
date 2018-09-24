e20ca2f08e08349b28004f42e17ca2ce5686a0cd
KatietheKoder <katieknecht@ymail.com>
2018-09-24 18:46:53 -0400

    List Rooms Complete

--- src/Components/RoomList.js
+++ src/Components/RoomList.js
@@ -7,6 +7,7 @@ class RoomList extends Component {
     super(props)
     this.state = {
       rooms:[]
+
       }
 
     this.roomsRef = this.props.firebase.database().ref('rooms');
@@ -25,29 +26,17 @@ class RoomList extends Component {
 
   render() {
 
-    const RoomForm = (
-
-      <form onSubmit={this.createRoom}>
-       <formGroup>
-         <inputGroup>
-           <formControl type="text" name="title" value={this.state.title} placeholder="New Room" onChange={this.handleChange} />
-           <button type="submit">Create</button>
-       </inputGroup>
-     </formGroup>
-     </form>
-
-    );
 
       const RoomList = this.state.rooms.map((room)=>{
-      <div key={room.key} onClick={ (e) => {this.selectRoom(room,e)} }> {room.name}.
-      <button className="deleteRoom" onClick= { (e) => {this.deleteRoom(room.key)} }>Delete</button>/>
 
+      return ( <li key={room.key} onClick={ (e) => {this.selectRoom(room,e)} }> {room.name}.
+
+      </li>)
 
   })
 
     return (
       <div className= 'SelectRoom'>
-        <ul>{RoomForm}</ul>
         <ul>{RoomList}</ul>
       </div>
     );
