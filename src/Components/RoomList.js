bd41b105db21675a5ac77cc19cb26a695b6575f3
wktdev <wktdev@gmail.com>
2018-10-01 13:22:25 -0700

    temp

--- src/Components/RoomList.js
+++ src/Components/RoomList.js
@@ -16,29 +16,60 @@ class RoomList extends Component {
   componentDidMount() {
 
     this.roomsRef.on('child_added', snapshot => {
-         const room = snapshot.val();
-         room.key = snapshot.key;
-         this.setState({ rooms: this.state.rooms.concat( room ) })
+      const room = snapshot.val();
+      room.key = snapshot.key;
+        this.setState(
+          { 
 
-          });
-       }
+            rooms: this.state.rooms.concat( room ),
+            formCache:""
 
+          }
+
+        )
+
+    });
+  }
+
+  createRoom=(event)=>{
+    event.preventDefault()
+
+    this.roomsRef.push({
+      name: this.state.formCache
+    });
+
+    this.setState({
+      formCache:""
+    });
+
+  }
+
+  handleChange=(event)=>{
+     this.setState({
+      formCache:event.target.value
+     });
+
+    
+  }
 
   render() {
 
-
-      const RoomList = this.state.rooms.map((room)=>{
-
-      return ( <li key={room.key} onClick={ (e) => {this.selectRoom(room,e)} }> {room.name}.
-
-      </li>)
-
-  })
+    const RoomList = this.state.rooms.map((room)=>{
+                        return (<li key={room.key}> {room.name}. </li>)
+                      })
 
     return (
+    <div>
+      <form onSubmit = {this.createRoom}>
+        <input type = "text" onChange = {this.handleChange} value={this.state.formCache}/>
+        <input type = "submit"/>
+      </form>
+
       <div className= 'SelectRoom'>
         <ul>{RoomList}</ul>
       </div>
+
+    </div>
     );
   }
 }
